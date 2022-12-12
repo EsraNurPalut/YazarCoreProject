86396822650e72b01c27fb8f36e4c50f91b9da0a
EsraNurPalut <nuresrapalut@outlook.com>
2022-12-12 18:56:12 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-1M6OMN39;Database=ProjectKutuphane; integrated security=true;");
+        }
+
+        public DbSet<Kitaplar> Kitaplars { get; set; }
+
+        public DbSet<Yazarlar> Yazarlars { get; set; }
+
+        public DbSet<Yayinevleri> Yayinevleris { get; set; }
+
+        public DbSet<Admin> Admins { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Belge> Belges { get; set; }
+
+    }
+}
